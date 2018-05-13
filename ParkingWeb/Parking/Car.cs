e4b49461aa39403154c596bf82d576552c3c081e
GuziyV <volodyaguziy3@gmail.com>
2018-05-13 16:36:39 +0300

    Add car, transactions, parking controllers

--- ParkingWeb/Parking/Car.cs
+++ ParkingWeb/Parking/Car.cs
@@ -2,7 +2,39 @@
 
 namespace Parking
 {
-    public class Class1
+    public class Car
     {
+        public Car(uint id, CarType type, decimal balance = 0)
+        {
+            Id = id;
+            Type = type;
+            Balance = balance;
+        }
+        public uint Id { get; private set; }
+
+        public readonly CarType Type;
+
+        public decimal Balance { get; private set; }
+
+        public decimal AddMoney(decimal value)
+        {
+            Balance += value;
+            return Balance;
+        }
+
+        public decimal GiveMoney(decimal value)
+        {
+            Balance -= value;
+            return Balance;
+        }
+
+    }
+
+    public enum CarType
+    {
+        Passenger,
+        Truck,
+        Bus,
+        Motorcycle
     }
 }
---
+++ ParkingWeb/ParkingWeb/Controllers/CarsController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Parking;
+
+namespace ParkingWeb.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Car")]
+    public class CarsController : Controller
+    {
+        // GET: api/Cars
+        [HttpGet]
+        public IEnumerable<Car> Get()
+        {
+            return Parking.Parking.Instance.GetAllCars();
+        }
+
+        // GET: api/Cars/5
+        [HttpGet("{id}", Name = "Get")]
+        public Car Get(int id)
+        {
+            return Parking.Parking.Instance.GetCarById(id);
+        }
+        
+        // POST: api/Car
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+        
+        // PUT: api/Cars/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+        
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
